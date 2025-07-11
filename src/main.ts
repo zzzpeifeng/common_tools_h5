1cee3d4663897e00d325730f7a1f987e8b155d05
zhangpeifeng <peifeng.zhang@shopline.com>
2025-07-11 18:37:49 +0800

    添加pinia、axiso

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import '@/style.css'
 import App from './App.vue'
 import router from '@/router'
 import 'amfe-flexible';
+import store from "@/store";
 
 const app = createApp(App);
+app.use(store)
 app.use(router)
+
 app.mount('#app');
