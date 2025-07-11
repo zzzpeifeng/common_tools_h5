f6fb2c73ea650f60070c5826f6749e6767276ba9
zhangpeifeng <peifeng.zhang@shopline.com>
2025-07-11 19:15:15 +0800

    添加pinia、axiso

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const showLogin = ref(true);
+// 存储用户名
+const username = ref('');
+// 存储密码
+const password = ref('');
+
+// 处理登录逻辑的函数
+const handleLogin = () => {
+  console.log('用户名:', username.value, '密码:', password.value);
+  // 可在此添加实际的登录请求逻辑
+  showLogin.value = false;
+};
+
+</script>
+
 <template>
   <div class="container flex flex-col h-screen">
     <div class="flex-1 flex w-100% h-30 justify-center items-center">
@@ -11,7 +30,31 @@
       </button>
     </div>
   </div>
+  <!-- 登录模态框 -->
+  <div v-if="showLogin" class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+    <div class="bg-white p-2 flex flex-col flex-auto items-center">
+      <h2 class="text-sm font-extralight text-black mb-2">商家登录</h2>
+      <input
+          v-model="username"
+          type="text"
+          placeholder="用户名"
+          class="w-[80%] p-1 border border-gray-300 mb-2 text-[0.4rem] font-extralight focus:border-black focus:ring-black"
+      />
+      <input
+          v-model="password"
+          type="password"
+          placeholder="密码"
+          class="w-[80%] p-1 border border-gray-300 mb-4 text-[0.4rem] font-extralight focus:border-black focus:ring-black"
+      />
+      <div class="flex justify-center">
+        <button class="bg-gray-300 text-black py-1 px-2 rounded-xs mr-2 text-[0.4rem] font-extralight" @click="showLogin = false">
+          取消
+        </button>
+        <button class="bg-black text-white py-1 px-2 rounded-xs text-[0.4rem] font-extralight" @click="handleLogin">
+          登录
+        </button>
+      </div>
+    </div>
+  </div>
 
 </template>
-<script setup lang="ts">
-</script>
